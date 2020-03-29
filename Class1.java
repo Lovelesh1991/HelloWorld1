4ad4228b873fa793fbbf5a7cff8556dbf7c74e49
Lovelesh <lovelesh1991@gmail.com>
2020-03-29 15:44:20 -0400

    First Commit

---
+++ Class1.java
@@ -0,0 +1,21 @@
+
+public class Class1 {
+	
+	/*public  static void firstMethod()
+	{
+		
+		System.out.println("I Am First");
+	}
+*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I Am First");
+	  //firstMethod();
+		
+		System.out.println("I Am Second");
+	}
+
+}
+
+
